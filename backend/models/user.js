cb540e9ecc1717d78557db2c11b3e4839be7abd3
Maria Kazmina <maryamm2017amm@yandex.ru>
2022-08-16 19:32:40 +0300

    fix

--- backend/models/user.js
+++ backend/models/user.js
@@ -43,7 +43,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).select('+password') // ищем пльзователя по email
+  return this.findOne( email ).select('+password') // ищем пльзователя по email
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
